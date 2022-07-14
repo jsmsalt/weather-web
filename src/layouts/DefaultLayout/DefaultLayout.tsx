1ef336c1067a0585b745783617d4f19e27c022be
José Morales <jsmsalt@outlook.com>
2022-07-14 14:59:36 -0300

    styles: Change layout styles

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,10 +1,19 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 export const DefaultLayout = () => {
     return (
-        <Container sx={{ py: 4 }}>
-            <Outlet />
-        </Container>
+        <Box
+            sx={{
+                display: "flex",
+                width: "100%",
+                alignItems: "stretch",
+                background: "linear-gradient(to top, #6dd5ed, #2193b0)",
+            }}
+        >
+            <Container maxWidth='md' sx={{ py: 4 }}>
+                <Outlet />
+            </Container>
+        </Box>
     );
 };
--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,9 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
     palette: {
         mode: "light",
+        background: {
+            default: "#eee",
+            paper: "#fff",
+        },
     },
 });
